5ab42479161c4ee9b98ec1150d82c0d2a775b4f2
malkesh-m <sahilkargutkar.sk@gmail.com>
2021-12-22 17:03:23 +0530

    ready for deployment

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const errorMiddleware = require("./middleware/error");
 const fileUpload = require("express-fileupload");
+const path = require("path");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -17,6 +18,12 @@ const order = require("./routes/orderRoute");
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 app.use(errorMiddleware);
 
